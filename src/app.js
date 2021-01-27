b92cbec157d1cfdfdff06713467955087db10d49
Paulo Carvalho <phcarvalhodev@gmail.com>
2021-01-27 11:39:37 -0300

    added test files

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`Hello ${process.env.name}!`)
+});
+
+app.listen(3000, () => {
+  console.log('Server listening at http://localhost:3000')
+})
